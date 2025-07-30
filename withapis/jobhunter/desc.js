e3ef3755950252432fec44b54c79b1803bf2066f
Maxwell Easter <maxwelleaster00@gmail.com>
2025-07-30 15:27:02 +0100

    add project description

---
+++ withapis/jobhunter/desc.js
@@ -0,0 +1,29 @@
+
+/* 
+
+core features: 
+    -user profile 
+    -jobs feed 
+    -settings
+
+pages (menu buttons) -> settings * jobs * profile  
+
+settings page: 
+    -dark mode toggle 
+    -layout switcher (compact, card)
+    -font family
+
+jobs page: 
+    -search bar (job title)
+    -search filter (type,  location)
+    -infinite scroll feed 
+    -job card (title, company, location, salary)
+    -job details card (description, requirements, company info & link)
+
+profile page: 
+    -bookmarks
+    -preferences
+    -user info (name, bio, tech stack) - stored on indexddb 
+    -clear bookmarks/preference button
+
+*/
