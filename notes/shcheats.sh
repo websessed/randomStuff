558fd5dbf69470d530bf56a4b6dc333fc9c3a974
Maxwell Easter <maxwelleaster00@gmail.com>
2025-08-04 15:42:23 +0100

    add more commands

--- notes/shcheats.sh
+++ notes/shcheats.sh
@@ -1,3 +1,4 @@
+#bash cheats 
 
 #search and open/jump a file in vscode 
 code $(find . -type f -name "filename.ext")   #single file 
@@ -8,3 +9,21 @@ code $(find . -type f -name "*filename*.ext") #multiple files
 cd ftl<Tab>/ftl<Tab>/ftl<Tab> #where ftl 👉 first three letters (of folder-name)
 #or 
 cd path/to/your/file/location
+
+
+#jump back to parent folder from child folder e.g parent/x/y/z
+cd ~/parent 
+#or 
+cd ../..
+
+
+#jump back to previous folder 
+cd - 
+
+
+#create multiple files within multiple folders at once 
+for f in file1 file2 file3 file4; do 
+    cp "$f" folder1
+    cp "$f" folder2
+done 
+
